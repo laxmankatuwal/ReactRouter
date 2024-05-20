526e37b93334cc25f8b02d6abfd3c67163b4dbf6
Laxman katuwal <katuwallaxman178@gmail.com>
2024-05-20 19:47:07 +0545

    Add files via upload

---
+++ LoadingSpinner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoadingSpinner = () => {
+  return (
+    <div className="d-flex justify-content-center spinner">
+      <div className="spinner-border" style={{ width: "5rem", height: "5rem" }}>
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
---
+++ WelcomeMsg.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WelcomeMessage = ({ onGetPostsClick }) => {
+  return (
+    <center className="Welcome-Msg">
+      <h1>there are no post</h1>
+    </center>
+  );
+};
+
+export default WelcomeMessage;
